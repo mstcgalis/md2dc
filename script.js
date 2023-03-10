3ceedb31a7b88ca1e4dcc916dc68b2382944d04a
Daniel Gális <danielgalis21@gmail.com>
2023-03-10 13:26:43 +0100

    bunch of things

--- script.js
+++ script.js
@@ -1,69 +1,66 @@
-const form = document.querySelector("#form");
-const input = document.querySelector("#input");
-const output = document.querySelector("#output");
-const copyStatus = document.querySelector("#copy-status");
+/* eslint-disable no-extra-semi */
+const form = document.querySelector('#form');
+const input = document.querySelector('#input');
+const output = document.querySelector('#output');
+const copyStatus = document.querySelector('#copy-status');
 
 const re = /\[(.+)\]\((http.+)\)/gm;
 
-let outputString = "";
-
-/**
- * Formats a string of markdown text to be displayed in the output element
- * 
- * @param {string} markdown 
- * @returns 
- */
-function formatMarkdown(markdown) {
-    // Replace newline characters with <br> elements
-    const formatted = markdown.replace(/\n/g, "<br>");
-  
-    // Return the formatted string
-    return formatted;
-  }
-
 /**
  * Converts markdown links to DC links
- * 
+ *
  * @param {string} mdString
  * @returns {string} dcString
  */
 function convertMd2Dc(mdString) {
-    let num = 0;
-    let links = [];
-    
-    const createLink = function(m, label, link) {
-        num += 1;
-        links.push(link)
-        const dcString = "**" + label + "**" + " (`link#" + num + "`)";
-        return dcString
-    };
+  let num = 0;
+  const links = [];
 
-    let linkList = "";
-    
-    const result = mdString.replaceAll(re, createLink);
-    
-    for (let i = 0; i < num; i++) {
-      if (i === 0) {
-        linkList += "\n\n";
-      }
-      linkList += "`link#" + (i+1) + "`" + ": " + links[i] + "\n";
-    };
+  // function to be used in the replaceAll method
+  function createLink(m, label, link) {
+    num += 1;
+    links.push(link);
+    const dcLink = `**${label}**(\`link#${num}\`)`;
+    return dcLink;
+  };
 
-    outputString = result + linkList;
+  let linkList = '';
 
-    output.value = outputString;
+  const textWithLinks = mdString.replaceAll(re, createLink);
+
+  for (let i = 0; i < num; i + 1) {
+    if (i === 0) {
+      linkList += '\n\n';
+    }
+    linkList += `\`link#${i + 1}\`: ${links[i]}\n`;
+  }
+
+  const dcString = textWithLinks + linkList;
+
+  return dcString;
 }
 
+/**
+ * Copies the output to the clipboard
+ */
+// eslint-disable-next-line no-unused-vars
 function copyOutput() {
   // Copy the output to the clipboard
   navigator.clipboard.writeText(output.value);
 
   // Change the copyStatus text to "Copied!"
-  copyStatus.innerHTML = "Copied!";
+  copyStatus.innerHTML = 'Copied!';
 
-}
+  // Change the copyStatus text back to "" after 1 second
+  setTimeout(() => {
+    copyStatus.innerHTML = '';
+  }, 1000);
+};
 
-form.addEventListener("submit", (event) => {
+// Add an event listener to the form
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  convertMd2Dc(input.value);
+  // const clean = DOMPurify.sanitize(input.value);
+  // Convert the Md inuput to DC output
+  output.value = convertMd2Dc(input.value);
 });
